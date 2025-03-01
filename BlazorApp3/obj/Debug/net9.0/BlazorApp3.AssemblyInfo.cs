0949b7c6453d1a095d56bef9617f32d4c93be6d6
DayKnight <elikouly@gmail.com>
2025-03-01 11:26:18 +0100

    Changes

--- BlazorApp3/obj/Debug/net9.0/BlazorApp3.AssemblyInfo.cs
+++ BlazorApp3/obj/Debug/net9.0/BlazorApp3.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3e390abef6e4c7e60686de9006eb119f1fb2216")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c4f7f3969ca1ce8f3a68440bfbeab09c38ec00e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
