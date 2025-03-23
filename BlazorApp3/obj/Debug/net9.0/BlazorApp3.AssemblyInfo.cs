0a9b359181a373d98416a4cb4429f842839a4d50
DayKnight <elikouly@gmail.com>
2025-03-23 20:09:21 +0100

    Fixed Auth not working

--- BlazorApp3/obj/Debug/net9.0/BlazorApp3.AssemblyInfo.cs
+++ BlazorApp3/obj/Debug/net9.0/BlazorApp3.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cabe842acfbe7733226513bd30d7f58a4e095173")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567472c254e1d37c4c2837678ff78437ee922d58")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
